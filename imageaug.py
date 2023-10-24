3058e6a248338c5e177f8952931e81c3f7bbc703
miaomiaolu-86 <148873887+miaomiaolu-86@users.noreply.github.com>
2023-10-24 23:18:21 +0800

    Update imageaug.py

--- imageaug.py
+++ imageaug.py
@@ -1,6 +1,13 @@
+#导入transforms函数对图像做预处理
 from torchvision import transforms
 
-
+'''
+将torch.tensor 转换为PIL图像
+图像尺寸转换到给定尺寸
+以0.5概率水平翻转给定的PIL图像
+将PIL图像转换为tensor
+用均值[0.5,0.5,0.5]和标准差[0.5,0.5,0.5]对图像做归一化处理
+'''
 def transform_for_training(image_shape):
     return transforms.Compose(
        [transforms.ToPILImage(),
