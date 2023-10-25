d781be36d4261b3c9eab3f9ea1150c3d57c0e27c
miaomiaolu-86 <148873887+miaomiaolu-86@users.noreply.github.com>
2023-10-25 11:04:54 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,16 +10,22 @@ import matplotlib.pyplot as plt
 
 
 def download(dir, url, dist=None):
+    #获取url最后一个/后的字符串
     dist = dist if dist else url.split('/')[-1]
+    '''format()控制字符串和变量显示方式
+    输出：从url地址开始下载字符串到目录dir下面'''
     print('Start to Download {} to {} from {}'.format(dist, dir, url))
     
     '''os.path子库，处理文件路径及信息
     获得文件路径'''
     download_path = os.path.join(dir, dist)
+    #判断该路径下对象是否是文件
     if os.path.isfile(download_path):
         print('File {} already downloaded'.format(download_path))
         return download_path
+    #若目录下不是文件，获取网页
     r = requests.get(url, stream=True)
+    #爬取数据，获得内容长度，
     total_size = int(r.headers.get('content-length', 0))
     block_size = 1024 * 1024
 
