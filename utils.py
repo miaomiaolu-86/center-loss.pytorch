2f484d2aaee470106bedcc223e9f2c00af755d50
miaomiaolu-86 <148873887+miaomiaolu-86@users.noreply.github.com>
2023-10-25 12:34:49 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 #Python标准库os库，包含几百个函数,常用路径操作、进程管理、环境参数等几类
 import os
 from math import ceil
-
+#导入进度条库
 from tqdm import tqdm
 import numpy as np
 import cv2
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 def download(dir, url, dist=None):
     #获取url最后一个/后的字符串
     dist = dist if dist else url.split('/')[-1]
+    
     '''format()控制字符串和变量显示方式
     输出：从url地址开始下载字符串到目录dir下面'''
     print('Start to Download {} to {} from {}'.format(dist, dir, url))
@@ -19,21 +20,27 @@ def download(dir, url, dist=None):
     '''os.path子库，处理文件路径及信息
     获得文件路径'''
     download_path = os.path.join(dir, dist)
+    
     #判断该路径下对象是否是文件
     if os.path.isfile(download_path):
         print('File {} already downloaded'.format(download_path))
         return download_path
-    #若目录下不是文件，获取网页
+        
+    #若目录下不是文件，获取网页的内容
     r = requests.get(url, stream=True)
     #爬取数据，获得内容长度，
     total_size = int(r.headers.get('content-length', 0))
     block_size = 1024 * 1024
 
+    #以写二进制的方式打开该路径下的文件
     with open(download_path, 'wb') as f:
         for data in tqdm(
-                r.iter_content(block_size),
-                total=ceil(total_size//block_size),
+            #迭代读取文件，每次读取1024 * 1024大小的数据块    
+            r.iter_content(block_size),
+            #向上取整
+            total=ceil(total_size//block_size),
                 unit='MB', unit_scale=True):
+            #信息写入到文件        
             f.write(data)
     print('Downloaded {}'.format(dist))
     return download_path
